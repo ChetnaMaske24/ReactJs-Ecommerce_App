79b637276b7bacf0b4930bf79b5d5ee9f76a83ad
Chetna <cmaske580@gmail.com>
2024-06-17 23:35:26 +0530

    banner

--- ecommerce_app/src/pages/Home/index.js
+++ ecommerce_app/src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SliderBanner from './slider/index';
 import CatSlider from '../../components/catSlider';
+import Banners from '../../components/banners';
 
 function index() {
   return (
     <div>
         <SliderBanner />
         <CatSlider />
+        <Banners />
     </div>
   )
 }
