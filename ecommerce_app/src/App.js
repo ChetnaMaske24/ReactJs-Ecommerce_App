0ce65eaf22c957d51d4110b95255f4f98fdf3828
Chetna <cmaske580@gmail.com>
2024-06-19 20:22:36 +0530

    Coded upto Footer Component

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -1,13 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/header/header';
-import Home from './pages/Home/index';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/header/header";
+import Home from "./pages/Home/index";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
-    <Header />
-    <Home />
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
