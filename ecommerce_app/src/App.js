d9dbce6ae5ecce8beced66b27894c22639b23d19
Chetna <cmaske580@gmail.com>
2024-06-21 20:56:43 +0530

    404 page/ NotFound page

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -1,18 +1,32 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Header from "./components/header/header";
-import Home from "./pages/Home/index";
-import Footer from "./components/footer/footer";
+import React, { useEffect, useState, createContext } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import './responsive.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Home from './pages/Home/index';
 import Listing from './pages/Listing';
+import NotFound from './pages/NotFound';
+import DetailsPage from './pages/Details';
+
 
 function App() {
+
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-      <Listing />
-    </>
+    
+    data.productData.length !== 0 &&
+    <BrowserRouter>
+        <Header data={data.productData} />
+        <Routes>
+          <Route exact={true} path="/" element={<Home data={data.productData} />} />
+          <Route exact={true} path="/cat/:id" element={<Listing data={data.productData} single={true} />} />
+          <Route exact={true} path="/cat/:id/:id" element={<Listing data={data.productData} single={false} />} />
+          <Route exact={true} path="*" element={<NotFound />} />
+        </Routes>
+       <Footer/>
+    </BrowserRouter>
   );
 }
 
