9fd64748763bf0a6caa10f57abc40b0af3c1b74d
Chetna <cmaske580@gmail.com>
2024-06-24 12:52:05 +0530

    Data

--- ecommerce_app/src/App.js
+++ ecommerce_app/src/App.js
@@ -1,34 +1,81 @@
-import React, { useEffect, useState, createContext } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-// import './responsive.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState, createContext } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Home from './pages/Home/index';
-import Listing from './pages/Listing';
-import NotFound from './pages/NotFound';
-import DetailsPage from './pages/Details';
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./pages/Home/index";
+import About from "./pages/About/index";
+import Listing from "./pages/Listing";
+import NotFound from "./pages/NotFound";
+import DetailsPage from "./pages/Details";
+import axios from "axios";
+import Loader from "./assets/images/loading.gif";
 
+import data from "./data";
+
+const MyContext = createContext();
 
 function App() {
+  const [productData, setProductData] = useState([]);
+
+  const [isLoading, setIsloading] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // getData('http://localhost:5000/productData');
+
+    const is_Login = localStorage.getItem("isLogin");
+    setIsLogin(is_Login);
+
+    setTimeout(() => {
+      setProductData(data[1]);
+      setIsloading(false);
+    }, 3000);
+  }, []);
 
   return (
-    
-    data.productData.length !== 0 &&
-    <BrowserRouter>
-        <Header data={data.productData} />
-        <Routes>
-          <Route exact={true} path="/" element={<Home data={data.productData} />} />
-          <Route exact={true} path="/cat/:id" element={<Listing data={data.productData} single={true} />} />
-          <Route exact={true} path="/cat/:id/:id" element={<Listing data={data.productData} single={false} />} />
-          <Route exact={true} path="/product/:id" element={<DetailsPage data={data.productData} />} />
-          <Route exact={true} path="*" element={<NotFound />} />
-        </Routes>
-       <Footer/>
-    </BrowserRouter>
+    data.productData.length !== 0 && (
+      <BrowserRouter>
+        <MyContext.Provider value={value}>
+          {isLoading === true && (
+            <div className="loader">
+              <img src={Loader} />
+            </div>
+          )}
+
+          <Header data={data.productData} />
+          <Routes>
+            <Route
+              exact={true}
+              path="/"
+              element={<Home data={data.productData} />}
+            />
+            <Route
+              exact={true}
+              path="/cat/:id"
+              element={<Listing data={data.productData} single={true} />}
+            />
+            <Route
+              exact={true}
+              path="/cat/:id/:id"
+              element={<Listing data={data.productData} single={false} />}
+            />
+            <Route
+              exact={true}
+              path="/product/:id"
+              element={<DetailsPage data={data.productData} />}
+            />
+            <Route exact={true} path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </MyContext.Provider>
+      </BrowserRouter>
+    )
   );
 }
 
 export default App;
+
+export { MyContext };
